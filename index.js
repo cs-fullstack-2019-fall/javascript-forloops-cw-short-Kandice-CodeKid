f7b80d55610a5c8caf7555684cf979c32e2c063b
autumn-ragland <raglanda998@gmail.com>
2019-08-08 15:56:20 -0500

    Graded 08-08-2019 15:54:40

--- index.js
+++ index.js
@@ -12,19 +12,19 @@
 //Use a for loop to print numbers 3- through 8
 //variable = -3
 
-// for (var i=(0-3); i<=5; i=i+1)
-// {console.log(i)}
-
+for (var i=(0-3); i<=5; i=i+1)
+{console.log(i)}
+// NOTE : It should print -3 through 8 not -3 though 5. Super close!! But be sure to read the directions
 //Exercise 2
 //Use a loop to print 0 to 50. Once it gets to 10, print "TEN!!!" instead of the number
 
-// for (var x=0; x<=50; x=x+1)
-// {
-//     if (x===10)
-//         console.log("TEN!!!");
-//     else
-//         console.log(x)
-// }
+for (var x=0; x<=50; x=x+1)
+{
+    if (x===10)
+        console.log("TEN!!!");
+    else
+        console.log(x)
+}
 //Exercise 3
 // Prompt the user to enter a number. Prompt the user to enter another number. Create a for loop that counts from the first number to the second number.
 
